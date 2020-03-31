f813e3043e78f2bc647948a04e3a9e87b714ebca
devin-kim <devin.kim@kakaocorp.com>
2020-04-01 00:48:04 +0900

    solve https://www.codewars.com/kata/5d5ee4c35162d9001af7d699/train/javascript

---
+++ test/sumOfMinimums.js
@@ -0,0 +1,13 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+function sumOfMinimums(arr) {
+  return arr.map(subArr => {
+    return Math.min.apply(null, subArr);
+  }).reduce((a, b) => a + b);
+}
+
+describe("Basic tests", () => {
+  it("Testing for [[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]", () => assert.strictEqual(sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]]), 9));
+  it("Testing for [[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]", () => assert.strictEqual(sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]]), 76));
+});
