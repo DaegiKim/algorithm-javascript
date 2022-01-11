371b11889af3255d08ab0188664a89a35b920128
devin-kim <devin.kim@kakaocorp.com>
2022-01-11 14:49:51 +0900

    solve https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/javascript

---
+++ test/Mumbling.js
@@ -0,0 +1,22 @@
+const chai = require("chai");
+const assert = chai.assert;
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function accum(s) {
+  return s.split('')
+    .map((value, index) => value.repeat(index + 1))
+    .map(value => capitalizeFirstLetter(value.toLowerCase()))
+    .join('-')
+}
+
+describe("accum",function() {
+  it("Basic tests",function() {
+    assert.equal(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+    assert.equal(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+    assert.equal(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+    assert.equal(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+    assert.equal(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+  })})
